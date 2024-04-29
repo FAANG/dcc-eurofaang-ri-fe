c48d90715b6b6e7385503e87f857697241aed09c
Alexey Sokolov <alexey@ebi.ac.uk>
2024-04-29 07:37:04 +0100

    Removing guard

--- eurofaang-ri/src/app/app.routes.ts
+++ eurofaang-ri/src/app/app.routes.ts
@@ -6,7 +6,7 @@ import {TnaComponent} from "./tna/tna.component";
 
 export const routes: Routes = [
   { path: 'login', component: UserLoginComponent },
-  { path: 'user-profile/:id', component: UserProfileComponent, canActivate: [authGuard] },
+  { path: 'user-profile/:id', component: UserProfileComponent},
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   {path: 'tna/new', component: TnaComponent},
   {path: 'tna/edit/:id', component: TnaComponent},
